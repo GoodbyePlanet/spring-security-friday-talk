d31fccba75a5b199aad97c9ac04db7de9b05c7f6
Nemanja <nemanja.vasic@productdock.com>
2024-09-30 23:19:25 +0200

    Initial setup

---
+++ src/main/java/com/pd/spring_security_friday_talk/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.pd.spring_security_friday_talk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+        throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class);
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+        throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/pd/spring_security_friday_talk/SpringSecurityFridayTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.spring_security_friday_talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFridayTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
