45360d086b41d4fec0965c96b9fea1778c11e72e
nemanja.vasic.ext <nemanja.vasic.ext@adevinta.com>
2025-02-22 02:16:52 +0100

    Add mariaDB

--- auth-server/src/main/java/com/app/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/com/app/auth_server/AuthServerApplication.java
@@ -1,11 +1,47 @@
 package com.app.auth_server;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class AuthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner runner(RegisteredClientRepository registeredClientRepository) {
+        return args -> {
+            RegisteredClient existingConfidentialClient = registeredClientRepository.findByClientId("confidential-client");
+
+            if (existingConfidentialClient == null) {
+                RegisteredClient confidentialClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("confidential-client")
+                        .clientSecret("{noop}secret")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                        .redirectUri("http://client.local:8080/login/oauth2/code/confidential-client")
+                        .scope(OidcScopes.OPENID)
+                        .scope(OidcScopes.PROFILE)
+                        .scope("contacts.read")
+                        .scope("contacts.write")
+                        .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                        .build();
+
+                registeredClientRepository.save(confidentialClient);
+            }
+        };
     }
 }
--- auth-server/src/main/java/com/app/auth_server/config/AuthorizationServerConfiguration.java
+++ auth-server/src/main/java/com/app/auth_server/config/AuthorizationServerConfiguration.java
@@ -1,45 +1,55 @@
 package com.app.auth_server.config;
 
+import com.app.auth_server.jpa.service.authorization.JpaAuthorizationService;
+import com.app.auth_server.jpa.service.authorizationconsent.JpaAuthorizationConsentService;
+import com.app.auth_server.jpa.service.client.JpaClientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
-import java.util.UUID;
-
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthorizationServerConfiguration {
 
+    private final JpaAuthorizationService authorizationService;
+    private final JpaAuthorizationConsentService authorizationConsentService;
+    private final JpaClientRepository registeredClientRepository;
+
     @Bean
-    @Order(1)
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
             throws Exception {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
                 OAuth2AuthorizationServerConfigurer.authorizationServer();
 
         http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
-                .with(authorizationServerConfigurer, (authorizationServer) ->
-                        authorizationServer.oidc(Customizer.withDefaults()))
+                .with(authorizationServerConfigurer, (authorizationServer) -> {
+                    authorizationServer.authorizationService(authorizationService);
+                    authorizationServer.authorizationConsentService(authorizationConsentService);
+                    authorizationServer.registeredClientRepository(registeredClientRepository);
+                    authorizationServer.oidc(Customizer.withDefaults());
+                })
                 .authorizeHttpRequests((authorize) ->
                         authorize.anyRequest().authenticated())
                 // Redirect to the login page when not authenticated from the
@@ -78,27 +88,24 @@ public class AuthorizationServerConfiguration {
     }
 
     @Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient confidentialClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("confidential-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://client.local:8080/login/oauth2/code/confidential-client")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .scope("contacts.read")
-                .scope("contacts.write")
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-        return new InMemoryRegisteredClientRepository(confidentialClient);
-    }
-
-    @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().issuer("http://auth.local:9000").build();
     }
+
+//    @Bean
+//    public JdbcOAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate,
+//                                                               RegisteredClientRepository registeredClientRepository) {
+//        return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+//    }
+//
+//    @Bean
+//    public JdbcRegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+//        return new JdbcRegisteredClientRepository(jdbcTemplate);
+//    }
+//
+//    @Bean
+//    public JdbcOAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate,
+//                                                                             RegisteredClientRepository registeredClientRepository) {
+//        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
+//    }
 }
